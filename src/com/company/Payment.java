cbfd9e1e0fed4575aa28d6009bc52446e1421369
Jacen <jacen19931@gmail.com>
2018-07-17 14:38:53 +0300

    start

---
+++ src/com/company/Payment.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Date;
+
+public class Payment {
+
+    private double change;
+    private Date date;
+    private Individual source;
+    private Individual target;
+
+    Payment(double change, Date date, Individual source, Individual target) {
+        this.change = change;
+        this.date = date;
+        this.source = source;
+        this.target = target;
+    }
+
+    public double getChange() {
+        return change;
+    }
+
+    public void setChange(double change) {
+        this.change = change;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Individual getSource() {
+        return source;
+    }
+
+    public void setSource(Individual source) {
+        this.source = source;
+    }
+
+    public Individual getTarget() {
+        return target;
+    }
+
+    public void setTarget(Individual target) {
+        this.target = target;
+    }
+}
