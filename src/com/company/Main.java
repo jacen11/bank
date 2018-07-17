f468fa507c6d0c9a4c9ff6e02741b31ea93bed15
jaizet <3978174@gmail.com>
2018-07-17 21:17:28 +0300

    Bank foundation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
 
-import com.company.Bank.PoorBank;
-import com.company.Customer.Customer;
-import com.company.Customer.PersonalCustomer;
+import com.company.bank.PoorBank;
+import com.company.customer.Customer;
+import com.company.customer.PersonalCustomer;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -12,13 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Customer client1 = new PersonalCustomer("login1","password1", BigDecimal.valueOf(1000),"1111111111");
         Customer client2 = new PersonalCustomer("login2","password2", BigDecimal.valueOf(2000),"2222222222");
-        Customer client3 = new PersonalCustomer("login3","password3", BigDecimal.valueOf(3000),"3333333333");
-        Map<String,Customer> clients = new HashMap<>();
-        clients.put(client1.getLogin(),client1);
-        clients.put(client2.getLogin(),client2);
-        clients.put(client3.getLogin(),client3);
-        PoorBank bank = new PoorBank(clients);
-        bank.createTask(client1,client2,BigDecimal.valueOf(500));
+        PoorBank.solveTask(client1,client2,BigDecimal.valueOf(500));
         System.out.println(client1.getCash());
         System.out.println(client2.getCash());
     }
